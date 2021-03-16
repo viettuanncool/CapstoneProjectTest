76e87253579d13db0becc783c7edbaf605d00858
nguyenvt <nguyenvt@sheridancollege.ca>
2021-03-16 11:33:47 -0400

    second commit

---
+++ Tests/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using CapstoneProject.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.HomeController
+{
+    [TestClass]
+    class HomeControllerTest
+    {
+        
+        [TestMethod]
+        public void StringInputCheckTestRegular(string input)
+        {
+            CapstoneProject.Controllers.HomeController con = new CapstoneProject.Controllers.HomeController();
+            
+            //Arrange
+            bool resultFromCall;
+            bool expectedResult = true;
+            
+            //Act
+            resultFromCall = con.StringInputCheck("Oakville");
+
+            //Assert
+            Assert.AreEqual(expectedResult, resultFromCall);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void StringInputCheckInvalidLength_ThrowsException_Check()
+        {
+            CapstoneProject.Controllers.HomeController con = new CapstoneProject.Controllers.HomeController();
+
+            //Arrange
+            string testString = "OakvilleOakvilleOakvilleOakville";
+
+            //Act
+            con.StringInputCheck(testString);
+
+            //Assert
+            
+
+        }
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeControllerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
